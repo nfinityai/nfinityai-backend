2b23b3902161cd1aa418fd46d616153bc90b4f3f
GolemXIV <golemxiv@noreply.localhost>
2024-07-18 08:35:29 +0000

    fix naming

--- backend_api/src/backend_api/api/router.py
+++ backend_api/src/backend_api/api/router.py
@@ -6,7 +6,7 @@ api_router = APIRouter()
 api_router.include_router(
     auth.router, prefix="/auth", tags=["SIWE Authentication Endpoints"]
 )
-api_router.include_router(users.router, prefix="/users", tags=["User Endpoints"])
-api_router.include_router(balance.router, prefix="/users", tags=["User Endpoints"])
-api_router.include_router(transactions.router, prefix="/users", tags=["User Endpoints"])
+api_router.include_router(users.router, prefix="/user", tags=["User Endpoints"])
+api_router.include_router(balance.router, prefix="/user", tags=["User Endpoints"])
+api_router.include_router(transactions.router, prefix="/user", tags=["User Endpoints"])
 api_router.include_router(model_providers.router)
